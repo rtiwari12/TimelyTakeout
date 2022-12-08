b96a0e07a3c661787f7099a55c04c1b1a08a625a
rtiwari12 <113547817+rtiwari12@users.noreply.github.com>
2022-12-08 16:30:34 -0500

    Add files via upload

--- flask-app/src/employees/employees.py
+++ flask-app/src/employees/employees.py
@@ -1,1 +1,58 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
 
+
+employees = Blueprint('employees', __name__)
+
+# Get employee detail for employee with particular employeeID
+@employees.route('/employee/<userID>', methods=['GET'])
+def get_employee(userID):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a certain employee
+    cursor.execute('select * from Employee where employeeID = {0}'.format(userID))
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# Get all transactions involving the specified employee
+@employees.route('/employee/<userID>/balance', methods=['GET'])
+def get_employee_balance(userID):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for transactions
+    cursor.execute('select * from Payment where EmployeeID = {0}'.format(userID))
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
