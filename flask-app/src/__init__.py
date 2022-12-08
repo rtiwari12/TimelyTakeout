af9e6983287f8b0de349509b4b0a5dc041737732
rtiwari12 <113547817+rtiwari12@users.noreply.github.com>
2022-12-08 16:31:59 -0500

    Add files via upload

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -3,8 +3,9 @@
 from flask import Flask
 from flaskext.mysql import MySQL
 
-# create a MySQL object that we will use in other parts of the API
+# MySQL object that we will use in other parts of the API
 db = MySQL()
+
 
 def create_app():
     app = Flask(__name__)
@@ -14,24 +15,25 @@ def create_app():
     # extensions or your application
     app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
 
-    # these are for the DB object to be able to connect to MySQL. 
+    # for the DB object to be able to connect to MySQL.
     app.config['MYSQL_DATABASE_USER'] = 'webapp'
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'classicmodels'  # Change this to your DB name
-
+    app.config['MYSQL_DATABASE_DB'] = 'TimelyTakeout'
     # Initialize the database object with the settings above. 
     db.init_app(app)
     
     # Import the various routes
-    from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.consumers.consumers import consumers
+    from src.employees.employees  import employees
+    from src.store.store import store
+    from src.store_mgr.store_mgr import store_mgr
 
     # Register the routes that we just imported so they can be properly handled
-    app.register_blueprint(views,       url_prefix='/classic')
-    app.register_blueprint(customers,   url_prefix='/classic')
-    app.register_blueprint(products,    url_prefix='/classic')
+    app.register_blueprint(consumers,   url_prefix='/cns')
+    app.register_blueprint(employees,    url_prefix='/emp')
+    app.register_blueprint(store,  url_prefix='/str')
+    app.register_blueprint(store_mgr,   url_prefix='/stmgr')
 
     return app
