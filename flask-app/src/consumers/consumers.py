4938a7c531a2eab55968ed47946bf443f4027cd2
rtiwari12 <113547817+rtiwari12@users.noreply.github.com>
2022-12-08 19:56:57 -0500

    Add files via upload

--- flask-app/src/consumers/consumers.py
+++ flask-app/src/consumers/consumers.py
@@ -22,9 +22,24 @@ def get_customers():
 
 # Get customer detail for customer with particular userID
 @consumers.route('/consumer/<userID>', methods=['GET'])
-def get_customer(userID):
+def get_reviews(userID):
     cursor = db.get_db().cursor()
     cursor.execute('select * from Consumer where ConsumerID = {0}'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# View all reviews from database
+@consumers.route('/reviews', methods=['GET'])
+def get_customer():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Review')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -52,7 +67,7 @@ def review():
                 <label for="ratingID">Please enter a 6 digit number:</label><br>
                 <input type="text" id="ratingID" name="ratingID" value=""><br>
                 <h2>Leave Review</h2>
-                <label for="rating">Enter a rating (1-5):</label><br>
+                <label for="rating">Enter a rating (1-10):</label><br>
                 <input type="rating" id="Stock" name="rating" value=""><br>
                 <label for="description">Enter a description:</label><br>
                 <input type="text" id="description" name="description" value=""><br>
